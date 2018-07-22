57d5206fbc4e2c8e3f2e270cda7ca63604b22939
Oday Mansour <odaym@nsour.net>
2018-07-22 16:26:06 +0200

    Commented perlin smoke code

--- scenes/perlin_smoke.js
+++ scenes/perlin_smoke.js
@@ -16,16 +16,16 @@ var seed = d.getTime();
 
 // Seeting up smoke
 var smoke = [];
-var nr_smoke = Math.max(sz_X, sz_Y) * 2;
+var nr_smoke = Math.max(sz_X, sz_Y) * 2; // Number of particles to simulate
 if (debug) {
     nr_smoke = 100;
 }
 
 // Speed parameters
-var val_coordweight = 350;
-var val_moveslowdown = 2300;
+var val_coordweight = 350; // Smaller value leades to tighter pattern
+var val_moveslowdown = 2300; //  Smaller value leads to faster pattern movement
 
-
+// This function only runs once, at startup
 function setup() {
     
     var canvas = createCanvas(sz_X, sz_Y);
@@ -39,6 +39,7 @@ function setup() {
     
     noiseSeed(seed);
 
+    // Creating the particles
     for (i = 0; i < nr_smoke; i++) {
         smoke.push({
                 x: Math.floor(Math.random() * (sz_X+1)), 
@@ -48,6 +49,7 @@ function setup() {
 
 }
 
+// This function runs at each frame
 function draw() {
 
     // background("rgba(17,0,55,0.002)");
@@ -57,7 +59,7 @@ function draw() {
 
     var x_unit_vector = createVector(1, 0);
 
-    // To show force vectors
+    // To draw force vectors
     if (debug) {
         background(0);
         for (var y=0; y<sz_Y; y+=50) {
@@ -77,24 +79,29 @@ function draw() {
     }
 
     if (debug) {
-        stroke("rgba(255, 255, 255, 1)")
+        stroke("rgba(255, 255, 255, 1)") // solid particles
     } else {
-        stroke("rgba(255, 255, 255, 0.005)")
+        stroke("rgba(255, 255, 255, 0.005)") // Very transparent particles, trails only show when a lot of dots overlap
     }
 
     for (i = 0; i < nr_smoke; i++) {
         ellipse(smoke[i].x, smoke[i].y, 1, 1);
+        // Interpolate the force vector at any given point
+        // the noise function takes in two or three parameters, in this case I'm using three:
+        // First two parameters are particle position (weighted)
+        // Third parameter is "time", also weighted
         var angle = noise(smoke[i].x/val_coordweight, smoke[i].y/val_coordweight, frameCount/val_moveslowdown) * 3 * PI;
         smoke[i].x += Math.cos(angle) * 1.5;
         smoke[i].y += Math.sin(angle) * 1.5;
         
+        // Redraw particle at a random location if it leaves the canvas
         if (smoke[i].x > sz_X | smoke[i].x < 0 | smoke[i].y > sz_Y | smoke[i].y < 0) {
             smoke[i].x = Math.random() * (sz_X + 1);
             smoke[i].y = Math.random() * (sz_Y + 1);
         }
     }
     
-
+    // draw the framerate in debug mode
     if (debug) {
         noStroke();
         fill("black");
